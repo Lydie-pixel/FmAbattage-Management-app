7ba39fe6388dc214689c89879db65b5507e53fb0
Lydie-pixel <fleursdesiles7@gmail.com>
2026-04-24 19:00:48 +0200

    Premier partie du front avec branchement API et mise en place du tableau de bord

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/database");
 const PORT = 3000;
 
 // 🔥 fichiers statiques (IMPORTANT pour ton front)
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "front")));
 
 // page d'accueil
 app.get("/", (req, res) => {
