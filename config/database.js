1ac21311677758e9f4e54cc351398ea16f785fdc
Lydie-pixel <fleursdesiles7@gmail.com>
2026-06-11 15:13:47 +0000

    Charement des packs de dependences manquant et changement de la connexion pour différencier prod et dev

--- config/database.js
+++ config/database.js
@@ -1,21 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
+
+const config = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql"
+};
+
+// SSL uniquement en production
+if (process.env.NODE_ENV === "production") {
+    config.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    };
+}
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-{
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
+    config
+);
 
-    dialectOptions: {
-    ssl: {
-        rejectUnauthorized: false
-        }
-    }
-});
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_PORT);
 
